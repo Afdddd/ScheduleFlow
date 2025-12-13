882bc945049e3b358a9961ce03b25f0bd98b8bba
Afdddd <kimdlsduq36@gmail.com>
2025-12-14 02:52:06 +0900

    feat: Project 및 관련 엔티티 추가
    프로젝트 관리 기능을 위한 기본 엔티티 정의
    - Project 엔티티: 프로젝트 정보 및 상태 관리
    - ProjectMembers 엔티티: 프로젝트 구성원 관리
    - ProjectStatus 상수: 프로젝트 상태(Enum) 정의

---
+++ src/main/kotlin/org/core/scheduleflow/domain/project/constant/ProjectStatus.kt
@@ -0,0 +1,8 @@
+package org.core.scheduleflow.domain.project.constant
+
+enum class ProjectStatus(val description: String) {
+
+    In_Progress("진행 중"),
+    ON_HOLD("보류"),
+    COMPLETE("완료")
+}
---
+++ src/main/kotlin/org/core/scheduleflow/domain/project/entity/ProjectMembers.kt
@@ -0,0 +1,27 @@
+package org.core.scheduleflow.domain.project.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.core.scheduleflow.domain.user.entity.User
+
+@Entity
+@Table(name = "project_members")
+class ProjectMembers(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    var project: Project,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    var user: User
+)
