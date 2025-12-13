d230c4fdb4042f8c3592c236b655f81a853040ea
Afdddd <kimdlsduq36@gmail.com>
2025-12-14 02:49:55 +0900

    feat: User 엔티티 및 Role 상수 추가
    User 엔티티에 사용자 정보 및 역할(Role) 필드 정의
    - 사용자 관리와 권한 분리를 위한 기반 코드 작성
    - Role 상수는 ADMIN과 STAFF로 구성

---
+++ src/main/kotlin/org/core/scheduleflow/domain/user/constant/Role.kt
@@ -0,0 +1,5 @@
+package org.core.scheduleflow.domain.user.constant
+
+enum class Role {
+    ADMIN,STAFF
+}
---
+++ src/main/kotlin/org/core/scheduleflow/domain/user/entity/User.kt
@@ -0,0 +1,45 @@
+package org.core.scheduleflow.domain.user.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.core.scheduleflow.domain.user.constant.Role
+import org.core.scheduleflow.global.entity.BaseEntity
+
+@Entity
+@Table(name = "users")
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role", nullable = false)
+    var userRole: Role = Role.STAFF,
+
+    @Column(name = "username", nullable = false)
+    var username: String,
+
+    @Column(name = "password", nullable = false)
+    var password: String,
+
+    @Column(name = "name", nullable = false)
+    var name: String,
+
+    @Column(name = "email", nullable = true)
+    var email: String? = null,
+
+    @Column(name = "phone", nullable = false)
+    var phone: String,
+
+    @Column(name = "position", nullable = false)
+    var position: String,
+
+): BaseEntity() {
+
+}
