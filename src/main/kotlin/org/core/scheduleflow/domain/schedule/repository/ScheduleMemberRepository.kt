76d08ffd3285a1acebe8b4ac36eb6f80b7e4ee2c
Afdddd <kimdlsduq36@gmail.com>
2025-12-21 23:56:15 +0900

    refactor: ScheduleMember 조회 로직 최적화
    - ScheduleMember와 연관된 User를 JOIN FETCH로 조회하도록 쿼리 개선
    - N+1 문제를 방지하고 성능 최적화를 위해 IN 쿼리를 활용

--- src/main/kotlin/org/core/scheduleflow/domain/schedule/repository/ScheduleMemberRepository.kt
+++ src/main/kotlin/org/core/scheduleflow/domain/schedule/repository/ScheduleMemberRepository.kt
@@ -2,8 +2,11 @@ package org.core.scheduleflow.domain.schedule.repository
 
 import org.core.scheduleflow.domain.schedule.entity.ScheduleMember
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ScheduleMemberRepository: JpaRepository<ScheduleMember, Long> {
-    fun findByScheduleIdIn(scheduleIds: List<Long>): List<ScheduleMember>
+    @Query("SELECT sm FROM ScheduleMember sm JOIN FETCH sm.user WHERE sm.schedule.id IN :scheduleIds")
+    fun findByScheduleIdIn(@Param("scheduleIds") scheduleIds: List<Long>): List<ScheduleMember>
 }
 
