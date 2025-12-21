25863b1790826790cc46485eb8e3f48fdc4b5a9c
Afdddd <kimdlsduq36@gmail.com>
2025-12-21 13:41:48 +0900

    feat: 프로젝트 CRUD API 완성
    - 프로젝트 전체 조회 및 세부 조회 API 추가
    - 프로젝트 업데이트 API 구현
    - 프로젝트 삭제 API 구현
    - Controller 레이어에 CRUD 관련 매핑 로직 추가
    - 연결된 서비스 로직 호출 및 응답 처리

--- src/main/kotlin/org/core/scheduleflow/domain/project/controller/ProjectController.kt
+++ src/main/kotlin/org/core/scheduleflow/domain/project/controller/ProjectController.kt
@@ -1,9 +1,16 @@
 package org.core.scheduleflow.domain.project.controller
 
 import org.core.scheduleflow.domain.project.dto.ProjectCreateRequest
+import org.core.scheduleflow.domain.project.dto.ProjectDetailResponse
+import org.core.scheduleflow.domain.project.dto.ProjectSummaryResponse
+import org.core.scheduleflow.domain.project.dto.ProjectUpdateRequest
 import org.core.scheduleflow.domain.project.service.ProjectService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,10 +21,35 @@ import org.springframework.web.bind.annotation.RestController
 class ProjectController(
     private val service: ProjectService
 ) {
+    @GetMapping
+    fun getProjects(): ResponseEntity<List<ProjectSummaryResponse>> {
+        return ResponseEntity.ok(service.findProjects())
+    }
+
+    @GetMapping("/{projectId}")
+    fun getProject(@PathVariable projectId: Long): ResponseEntity<ProjectDetailResponse> {
+        return ResponseEntity.ok(service.findProject(projectId))
+    }
+
     @PostMapping
     fun createProject(
         @RequestBody request: ProjectCreateRequest
     ): ResponseEntity<Long> = ResponseEntity.status(HttpStatus.CREATED).body(
         service.createProject(request)
     )
+
+    @PatchMapping("/{projectId}")
+    fun updateProject(
+        @PathVariable projectId: Long,
+        @RequestBody request: ProjectUpdateRequest): ResponseEntity<ProjectDetailResponse> {
+        return ResponseEntity.ok(service.updateProject(projectId, request))
+    }
+
+    @DeleteMapping("/{projectId}")
+    fun deleteProject(@PathVariable projectId: Long): ResponseEntity<Void> {
+        service.deleteProject(projectId)
+        return ResponseEntity.noContent().build()
+    }
+
+
 }
