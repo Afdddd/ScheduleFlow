befa6120d744389776011150f2504d6f7ba38368
Afdddd <kimdlsduq36@gmail.com>
2025-12-22 23:46:30 +0900

    feat: 스케줄 관련 DTO 클래스 추가
    - 스케줄 생성 및 수정 요청을 위한 ScheduleCreateRequest, ScheduleUpdateRequest 추가
    - 스케줄 요약 및 상세 응답을 위한 ScheduleSummaryResponse, ScheduleDetailResponse 추가
    - 스케줄 구성원 정보를 위한 ScheduleMemberDto 추가
    
    # Conflicts:
    #	backend/src/main/kotlin/org/core/scheduleflow/domain/schedule/dto/ScheduleCreateRequest.kt
    #	backend/src/main/kotlin/org/core/scheduleflow/domain/schedule/dto/ScheduleDetailResponse.kt
    #	backend/src/main/kotlin/org/core/scheduleflow/domain/schedule/dto/ScheduleMemberDto.kt
    #	backend/src/main/kotlin/org/core/scheduleflow/domain/schedule/dto/ScheduleSummaryResponse.kt
    #	backend/src/main/kotlin/org/core/scheduleflow/domain/schedule/dto/ScheduleUpdateRequest.kt

---
+++ backend/src/main/kotlin/org/core/scheduleflow/domain/schedule/dto/ScheduleDetailResponse.kt
@@ -0,0 +1,31 @@
+package org.core.scheduleflow.domain.schedule.dto
+
+import org.core.scheduleflow.domain.schedule.constant.ScheduleType
+import org.core.scheduleflow.domain.schedule.entity.Schedule
+import java.time.LocalDate
+
+data class ScheduleDetailResponse(
+    val id: Long,
+    val title: String,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val type: ScheduleType,
+    val projectId: Long?,
+    val members: List<ScheduleMemberDto>
+) {
+    companion object {
+        fun from(
+            schedule: Schedule,
+            members: List<ScheduleMemberDto>): ScheduleDetailResponse {
+            return ScheduleDetailResponse(
+                schedule.id!!,
+                schedule.title,
+                schedule.startDate,
+                schedule.endDate,
+                schedule.type,
+                schedule.project?.id,
+                members
+            )
+        }
+    }
+}
---
+++ backend/src/main/kotlin/org/core/scheduleflow/domain/schedule/dto/ScheduleUpdateRequest.kt
@@ -0,0 +1,14 @@
+package org.core.scheduleflow.domain.schedule.dto
+
+import org.core.scheduleflow.domain.schedule.constant.ScheduleType
+import java.time.LocalDate
+
+data class ScheduleUpdateRequest(
+    val title: String?,
+    val startDate: LocalDate?,
+    val endDate: LocalDate?,
+    val scheduleType: ScheduleType?,
+    val type: ScheduleType?,
+    val projectId: Long?,
+    val memberIds: List<Long>?
+)
